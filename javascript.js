8070522dc462d9b5d3d3af5690ea584a38b308a9
Anonymous-JWX <95593272+Anonymous-JWX@users.noreply.github.com>
2022-01-01 15:24:30 +1100

    .

---
+++ javascript.js
@@ -0,0 +1,41 @@
+var quotes = [
+  "Don't worry about what anybody else is going to do. <br>The best way to predict the future is to invent it.  <br><strong>-- Alan Kay</strong>",
+  "Keep away from people who try to belittle your ambitions. <br>Small people always do that, but the really great make you feel that you, too, <br>can become great. <br><strong>-- Mark Twain</strong>",
+  "To follow the path:<br>\
+    look to the master,<br>\
+    follow the master,<br>\
+    walk with the master,<br>\
+    see through the master,<br>\
+    become the master.<br>\
+-- Modern zen Poem",
+  "Everything should be made as simple as possible, but no simpler.<br>\
+-- Albert Einstein",
+  "Everybody makes their own fun.<br> \
+If you don't make it yourself, it ain't fun -- it's entertainment.<br>\
+-- David Mamet (as relayed by Joss Whedon)",
+  "Life moves pretty fast.<br>\
+ If you don't stop and look around once in a while, you could miss it.<br>\
+-- Ferris Bueller",
+  "For the things we have to learn before we can do them,<br> we learn by doing them.\
+-- Aristotle.",
+  "Only bad designers blame their failings on the users.<br>\
+-- Unknown",
+  "If I tell you I'm good, you would probably think I'm boasting.<br>\
+ If I tell you I'm no good, you know I'm lying.<br>\
+-- Bruce Lee",
+  "In theory, there's no difference between theory and practice.<br>\
+ But in practice, there is.<br>\
+-- Albert Einstein",
+  "千里之行，始于足下 <br>\
+-- 老子<br>\
+A journey of a thousand miles must begin with a single step.<br>\
+-- LaoTzu",
+  "We are the sum of our behaviours;<br>\
+ excellence therefore is not an act but a habit.<br>\
+-- Aristotle",
+];
+
+function newQuote() {
+  var randomnumber = Math.floor(Math.random() * quotes.length);
+  document.getElementById("quoteDisplay").innerHTML = quotes[randomnumber];
+}
